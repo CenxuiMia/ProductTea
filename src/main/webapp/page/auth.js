ad007f593c8810b78f1299fcce99d4d2da3fffad
Cenxui <cenxuilin@gmail.com>
2017-11-09 16:11:24 +0800

    add cognito user login

---
+++ src/main/webapp/page/auth.js
@@ -0,0 +1,14 @@
+let identityPool = 'ap-northeast-1:ab50eb1e-7005-4337-bf74-dae01c60a0cc'; //Cognito Identity Pool
+let region = 'ap-northeast-1';
+let poolId = 'ap-northeast-1_6cIsdfBKc'; //Cognito User Pool
+let clientId = '4ho3iodosfju9a96odrv3gavvb'; //Cognito User Pool App
+let appDomain = 'cens.auth.ap-northeast-1.amazoncognito.com';
+let endpoint = "https://9hssiictdl.execute-api.ap-northeast-1.amazonaws.com/prod"; //API Endpoint URL
+let authData = {
+    ClientId : clientId, 
+    AppWebDomain : appDomain,
+    TokenScopesArray : ['openid'],
+    RedirectUriSignIn : 'https://tw.hwangying.com', //CloudFront Distribution URL
+    RedirectUriSignOut : 'https://tw.hwangying.com' //CloudFront Distribution URL
+};
+
