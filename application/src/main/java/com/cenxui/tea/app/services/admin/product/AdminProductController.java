8e3e8a6677c12978fd7c9fdfae372741d16f354f
Cenxui <cenxuilin@gmail.com>
2017-11-25 13:45:26 +0800

    product list get short introduction

---
+++ application/src/main/java/com/cenxui/tea/app/services/admin/product/AdminProductController.java
@@ -0,0 +1,33 @@
+package com.cenxui.tea.app.services.admin.product;
+
+import com.cenxui.tea.app.services.CoreController;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class AdminProductController extends CoreController {
+    public static final Route getAllProducts = (Request request, Response response) -> {
+        //todo
+        throw new UnsupportedOperationException("not yet");
+    };
+
+    public static final Route getProduct = (Request request, Response response) -> {
+        //todo
+        throw new UnsupportedOperationException("not yet");
+    };
+
+    public static final Route addProduct = (Request request, Response response) -> {
+        //todo
+        throw new UnsupportedOperationException("not yet");
+    };
+
+    public static final Route updateProduct = (Request request, Response response) -> {
+        //todo
+        throw new UnsupportedOperationException("not yet");
+    };
+
+    public static final Route deleteProduct = (Request request, Response response) -> {
+        //todo
+        throw new UnsupportedOperationException("not yet");
+    };
+}
