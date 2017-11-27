0c6061c99e9976f30f63219d8d3c710c3bc48ed1
huaying <mtlisa42@gmail.com>
2017-11-27 14:54:26 +0800

    Get localStorage value and set to document value.

--- application/src/main/webapp/page/order.js
+++ application/src/main/webapp/page/order.js
@@ -12,7 +12,26 @@ $(document).ready(function () {
             console.info("index on signOut");
         }
     );
+
+    setInputWithUserData();
 });
+
+function setInputWithUserData() {
+    if (localStorage.getItem("lastName") !== null) {
+        console.info("LocalStorage! lastName: " + localStorage.lastName +
+            ", firstName: " + localStorage.firstName +
+            "\nphone: " + localStorage.phone +
+            ", address: " + localStorage.address);
+
+        document.getElementById("purchaser").setAttribute("value", localStorage.lastName + localStorage.firstName);
+        // document.getElementById("mail").setAttribute("value", localStorage.mail); //TODO get mail from localStorage
+        document.getElementById("phone").setAttribute("value", localStorage.phone);
+        document.getElementById("address").setAttribute("value", localStorage.address);
+    } else {
+        console.info("Query DB!");
+        //TODO query user data from DB
+    }
+}
 
 function addOrder() {
     let order = {};
