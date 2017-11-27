39308d4858c4b96171e8efa2859265ac0d2a6bee
huaying <mtlisa42@gmail.com>
2017-11-27 14:14:09 +0800

    Reorganize code.

--- application/src/main/webapp/page/order.js
+++ application/src/main/webapp/page/order.js
@@ -13,3 +13,34 @@ $(document).ready(function () {
         }
     );
 });
+
+function addOrder() {
+    let order = {};
+    order.purchaser = document.getElementById("purchaser").value;
+    order.mail = document.getElementById("mail").value;
+    order.phone = document.getElementById("phone").value;
+    order.receiver = document.getElementById("receiver").value;
+    order.address = document.getElementById("address").value;
+    order.products = [];
+    order.products[0] = document.getElementById("products").value;
+    order.money = document.getElementById("money").value;
+    order.comment = document.getElementById("comment").value;
+    let id_token = getToken();
+
+    $.ajax({
+        type : 'PUT',
+        url : orderEndpoint,
+        headers : {
+            Authorization : id_token
+        },
+        data: JSON.stringify(order),
+        success : function(response) {
+            console.log("message: " + response)
+
+        },
+        error : function(xhr, status, error) {
+            console.log( "error: " + error);
+        }
+    });
+
+}
