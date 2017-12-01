2382cd0c3f5349cf451df9509da10942390d73c9
huaying <mtlisa42@gmail.com>
2017-12-01 13:21:18 +0800

    Add addToCart func and localStorage.

--- application/src/main/webapp/page/product.js
+++ application/src/main/webapp/page/product.js
@@ -12,8 +12,10 @@ $(document).ready(function () {
             console.info("index on signOut");
         }
     );
+    // localStorage.removeItem("cartItems"); //For testing
 });
 
+let productDataAsKey;
 let name = getParameterByName("name");
 let version = getParameterByName("version");
 
@@ -25,12 +27,14 @@ $.ajax({
     url : productEndpoint + "/" + name + "/" + version,
     success : function(response) {
         console.log("product: " + response);
+        productDataAsKey = response;
 
         let data = JSON.parse(response);
         document.getElementById("smallImage").setAttribute("src", data.smallImage);
         document.getElementById("productName").innerHTML = data.productName;
+        document.getElementById("version").innerHTML = data.version;
         document.getElementById("introduction").innerHTML = data.introduction;
-        document.getElementById("price").innerHTML = "NT." + data.price;
+        document.getElementById("price").innerHTML = data.price;
         document.getElementById("video").setAttribute("src", data.video);
 
         let images = document.getElementById("images");
@@ -54,4 +58,30 @@ function getParameterByName(name) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function addToCart() {
+    let cartItems;
+    let selected = document.getElementById("count");
+    let count = parseInt(selected.options[selected.selectedIndex].value);
+    console.info("selected count = " + count);
+
+    if (localStorage.getItem("cartItems") === null) {
+        console.info("Init cartItems to localstorage");
+        cartItems = new Map();
+    } else {
+        console.info("Localstorage has cartItems");
+        cartItems = new Map(JSON.parse(localStorage.getItem("cartItems")));
+        console.info("Get old cartItems: " + JSON.parse(localStorage.getItem("cartItems")));
+        if (cartItems.has(productDataAsKey)) {
+            console.info("Just update cartItems to localstorage");
+            count += parseInt(cartItems.get(productDataAsKey));
+            console.info("count += " + count);
+        }
+    }
+    cartItems.set(productDataAsKey, count);
+    localStorage.setItem("cartItems", JSON.stringify(Array.from(cartItems.entries())));
+
+    console.info("localstorage getItem: " + localStorage.getItem("cartItems"));
+    console.info("JSON parse: " + JSON.parse(localStorage.cartItems));
 }
