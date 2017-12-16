db44d8995c504e975e7c9147c6d07febff8bc1a8
Cenxui <cenxuilin@gmail.com>
2017-12-16 22:15:59 +0800

    rename images to smallImages

--- application/src/main/java/com/cenxui/tea/app/config/ControllerConfig.java
+++ application/src/main/java/com/cenxui/tea/app/config/ControllerConfig.java
@@ -2,5 +2,5 @@ package com.cenxui.tea.app.config;
 
 public class ControllerConfig {
 
-    public static final boolean PRODUCT_CONTROLLER_CACHED = false;
+    public static final boolean PRODUCT_CONTROLLER_CACHED = false; //todo true in production
 }
