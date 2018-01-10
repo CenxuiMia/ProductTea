4412ec8c74f8573ea37cf8cee07ce890333afffc
huaying <mtlisa42@gmail.com>
2018-01-10 23:31:19 +0800

    Add new home page.

--- application/src/main/webapp/page/index.js
+++ application/src/main/webapp/page/index.js
@@ -12,5 +12,31 @@ $(document).ready(function () {
             console.info("index on signOut");
         }
     );
+
+    showSlides();
 });
 
+let slideIndex = 0;
+
+function showSlides(n) {
+    if (typeof n !== 'undefined') {
+        slideIndex = n;
+    }
+
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" dotActive", "");
+    }
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " dotActive";
+    slideIndex++;
+    setTimeout(showSlides, 3000);
+}
