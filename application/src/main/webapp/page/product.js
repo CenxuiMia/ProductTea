2f4d4cdcc148f0959f0480ec37ce2a9cf774003b
huaying <mtlisa42@gmail.com>
2018-02-04 17:35:46 +0800

    Update product page font size.

--- application/src/main/webapp/page/product.js
+++ application/src/main/webapp/page/product.js
@@ -36,7 +36,7 @@ $.ajax({
         let spec = data.spec.replace(/;/g, "<br>");
         document.getElementById("smallImage").setAttribute("src", data.smallImage);
         document.getElementById("productName").innerHTML = data.productName;
-        document.getElementById("version").innerHTML = data.version;
+        document.getElementById("version").innerHTML = data.version + versionName;
         document.getElementById("introduction").innerHTML = data.introduction;
         document.getElementById("spec").innerHTML = spec;
         document.getElementById("price").innerHTML = data.price;
